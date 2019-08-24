ee5379ba733886baffd48104fbf6c97e40b6e8de
LotteWong <LotteWong21@gmail.com>
2019-08-24 17:49:39 +0800

    更新前端

--- frontend/pages/activity_info/activity_info.js
+++ frontend/pages/activity_info/activity_info.js
@@ -1,66 +1,105 @@
-// pages/activity_info/activity_info.js
+// pages/activity_info/activity_1/activity_1.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    account: 'u_id', // 使用用户标识
+    a_id: 'a_id',
+    title: '学术讲座 | 行业月系列活动 — 智慧城市',
+    time:'2019-09-01',
+    place: '大学城校区 · 学术大讲堂',
+    poster: '../../images/poster.jpg',
+    intro: '随着国家治理体系和治理能力的不断现代化，随着‘创新、协调、绿色、开放‘发展理念不断深入，城市被赋予新的内涵与要求。新一期行业月系列活动，我们将邀请资深行业专家，为您解读新型智慧城市的挑战与发展。工商管理学院作为华南地区管理教育的先行者，学院办学三十余年，秉承“接力工科优势、推进理论创新、培育管理精英、服务社会发展”的使命，坚持知识、能力、技能与素质一体化的复合型创新人才培养模式，形成“理论创新与实践、工程技术与管理、国际化与本土化相融合”的办学特色，培养了一大批远见卓识、务实进取的社会才俊和管理精英，赢得了“华南企业家的摇篮，商界精英的殿堂”的赞誉。目前，学院拥有工商管理和管理科学与工程两个一级学科博士点、博士后流动站，具有EMBA、MBA、ME、MPACC专业学位授予权。管理科学与工程学科是广东省唯一攀峰重点学科，工商管理学科是优势重点学科。拥有包括长江学者特聘教授、国家杰出青年科学基金获得者、国家“万人计划”哲学社会科学领军人才、中宣部文化名家暨“四个一批”人才等国家级高层次人才的师资队伍。拥有国内一流的教学设施与优美环境，拥有广泛的国际合作与交流网络。研究成果多次获得教育部、广东省哲学社会科学优秀成果奖一等奖和入选国家哲学社会科学成果文库。',
+    u_id: 'u_id', // 发布用户标识
+    user_name: '马化腾',
+    d_id: 'd_id',
+    dept_name: 'Tencent',
+    max: 100,
+    now: 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 页面加载活动
+  onLoad: function (q) {
+    console.log("页面加载活动");
+    console.log("跨界面传参为" + q.activity + ' ' + q.account);
+    wx.showLoading({
+      title: '努力加载中...',
+    });
+    this.setData({
+      a_id: q.activity,
+      account: q.account
+    });
+    this.loadAcitivity(q.activity);
+    wx.hideLoading();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  // 下拉更新活动
   onPullDownRefresh: function () {
-
+    console.log("下拉更新活动");
+    wx.showNavigationBarLoading();
+    setTimeout(() => {
+      this.loadAcitivity(this.a_id);
+      wx.hideNavigationBarLoading();
+      wx.stopPullDownRefresh();
+    }, 2000);
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 后端获取活动
+  loadAcitivity: function (id) {
+    var that = this;
+    wx.request({
+      url: "TODO: $某个活动接口地址", // 接口地址
+      method: 'GET', // 请求方法
+      header: {
+        'content-type': 'application/json' // 默认类型
+      },
+      // 成功
+      success: function (res) {
+        console.log("后端获取活动 √成功", "TODO: $某个活动返回数据");
+        that.setData("TODO: $某个活动返回数据");
+      },
+      // 失败
+      fail: function (err) {
+        console.log("后端获取活动 ×失败", err);
+      }
+    });
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 是否已经报名
+  isAlreadyEnroll: function(id) {
+    var that = this;
+    wx.request({
+      url: "TODO: $是否报名接口地址", // 接口地址
+      method: 'GET', // 请求方法
+      header: {
+        'content-type': 'application/json' // 默认类型
+      },
+      // 成功
+      success: function (res) {
+        console.log("后端是否报名 √成功", "TODO: $是否报名返回数据");
+        that.setData("TODO: $是否报名返回数据");
+      },
+      // 失败
+      fail: function (err) {
+        console.log("后端是否报名 ×失败", err);
+      }
+    });
+  },
 
-  }
+  // 报名参加活动
+  signUp: function (e) {
+    var tmp = this.data.now;
+    tmp += 1;
+    var isIn = isAlreadyEnroll(account);
+    if (tmp <= this.data.max && !isIn) {
+      this.setData({ now: tmp });
+      // TODO: 向sign表插数据
+      // ......
+      console.log("报名成功");
+    }
+    else if(isIn){
+      console.log("已经报名");
+    }
+    else {
+      console.log("报名失败");
+    }
+  },
 })
--- frontend/pages/activity_list/activity_list.js
+++ frontend/pages/activity_list/activity_list.js
@@ -1,66 +1,61 @@
-// activity.js
-
-// TODO: 加标签加分类
-// TODO: 右边按钮和文本调成相对位置
-// TODO: 解决图片问题
+// activity_list.js
 
 var app = getApp()
 
 Page({
   data: {
+    // 当前用户
+    account: 'u_id',
+
+    // 活动数据
     activities: [
       {
-        id: 1000000,
-        title: "微软亚洲研究院创新论坛2019",
-        time: "2019-08-22",
-        address: "北京微软大厦",
-        content: "创新汇",
-        host: "微软亚研",
-        left: 7,
-        total: 21,
-        isIn: false,
-      },
-      {
-        id: 1000001,
-        title: "2019年二十一世纪的计算大会",
-        time: "2019-08-30",
-        address: "上海世博中心",
-        content: "二十一世纪的计算大会",
-        host: "微软亚研",
-        left: 7,
-        total: 21,
-        isIn: false,
+        a_id: 'a_id', // 活动标识
+        title: 'title', // 活动标题
+        time: 'time', // 活动时间
+        place:'place', // 活动地点
+        poster:'../../images/poster.jpg', // 活动海报
+        intro: 'intro', // 活动介绍
+        u_id: 'u_id', // 发布用户标识
+        user_name: "user_name", // 发布用户名字
+        d_id: 'd_id', // 发布组织标识
+        dept_name: "dept_name", // 发布组织名字
+        max: 100, // 上限人数
+        now: 0, // 报名人数
       }
     ],
-
-    thumbnailUrl: "../../images/",
     
-    indicatorDots: true,
+    // 轮播属性
+    /*indicatorDots: true,
     vertical: false,
     autoplay: true,
     circular: false,
     interval: 2000,
     duration: 500,
     previousMargin: 0,
-    nextMargin: 0,
+    nextMargin: 0,*/
 
+    // 分类选项
     tag: ['全部显示', '文娱活动', '体育赛事', '知识讲座', '学科竞赛'],
     idxOfTag: 0,
     classification: '分类：全部显示',
+    
+    // 排序选项
     sort: ['按举办时间', '按上限人数', '按报名人数'],
     idxOfSort: 0,
     order: '排序：按举办时间',
+
+    // 搜索信息
+    SearchData: '',
+    SearchResult: [],
   },
 
-  // 加载活动列表
+  // 页面加载活动
   onLoad: function () {
-    console.log("加载活动列表");
-    // var that = this;
+    console.log("页面加载活动");
     wx.showLoading({
       title: '努力加载中...',
     });
-
-    // that.loadActivities();
     this.loadAcitivities();
     wx.hideLoading();
   },
@@ -68,145 +63,94 @@ Page({
   // 下拉更新活动
   onPullDownRefresh: function () {
     console.log("下拉更新活动");
-    // var that = this;
     wx.showNavigationBarLoading();
-
     setTimeout(() => {
-      // that.loadActivities();
       this.loadAcitivities();
       wx.hideNavigationBarLoading();
       wx.stopPullDownRefresh();
-    }, 2000)
+    }, 2000);
   },
 
-  // 后端获取数据
+  // 后端获取活动
   loadAcitivities: function () {
     var that = this;
     wx.request({
       url: "http://localhost:8888/dbpractice/dbadmin/listactivity", // 接口地址
-      method: 'GET',
+      method: 'GET', // 请求方法
       header: {
-        'content-type': 'application/json' //默认值
+        'content-type': 'application/json' // 默认类型
       },
       // 成功
       success: function (res) {
-        console.log("加载活动列表 成功", res.data.activityList);
-        // TODO:
-        // that.setData({
-          // activities: res.data
-        // });
+        console.log("后端获取活动 √成功", res.data.activityList);
+        that.setData({
+          activities: res.data
+        });
       },
       // 失败
       fail: function (err) {
-        console.log("加载活动列表 失败", err);
+        console.log("后端获取活动 ×失败", err);
       }
-    })
-  },
-
-  // 活动详情
-  showActivity: function (event) {
-    console.log(event.currentTarget.id)
-    console.log(!(event.currentTarget.id == ""))
-    console.log("../activity_info/activity_/" + event.currentTarget.id + "activity_" + event.currentTarget.id)
-
-    if (!(event.currentTarget.id == "")) {
-      console.log("存在该活动");
-      wx.navigateTo({
-        url: "../activity_info/activity_" + event.currentTarget.id + "/activity_" + event.currentTarget.id
-      });
-    } else {
-      console.log("没有该活动");
-    }
-
-    // TODO: blk传参问题
-    wx.navigateTo({
-      url: "../activity_info/activity_" + 1 + "/activity_" + 1
     });
   },
 
-  // 活动筛选
-  filterActivities: function() {
-    console.log("活动筛选");
+  // 跳转活动详情
+  showActivity: function (e) {
+    console.log("跳转活动索引 !" + e.currentTarget.dataset.id);
     wx.navigateTo({
-      url: "../classifier/classifier"
+      url: "../activity_info/activity_info?activity=" + e.currentTarget.dataset.id + "&account=" + this.data.account
     });
   },
 
-  // 活动排序
-  sortActivities: function() {
-    console.log("活动排序");
-    var that = this;
-    wx.request({
-      url: "http://localhost:8888/dbpractice/dbadmin/addactivity", // 接口地址
-      header: {
-        "Content-Type": "application/x-www-form-urlencoded" //默认值
-      },
-      method: "POST",
-      data: Util.json2Form({
-        id: 23333333,
-        title: "活动一",
-        date: 2019 - 7 - 29,
-        place: "华南理工大学",
-        desc: "测试",
-        sponsor: "活在华工小组",
-        certified: 1
-      }),
-      complete: function (res) {
-        // TODO:
-        // that.setData({
-        // ...
-        // });
-        // 失败
-        if (res == null || res.data == null) {
-          console.error('网络请求失败');
-          return;
-        }
-        // 成功
-        else {
-          console.log(res.data);
-          return;
-        }
-      }
-    })
-  },
-
-  // 参与退出活动
-  handleActivity: function() {
-    // TODO: btn传参问题
-    console.log(this.data.activities[0].isIn);
-
-    if(this.data.activities[0].isIn)
-      console.log("退出活动");
-    else
-      console.log("参与活动");
-    var that = this;
-    that.data.activities[0].isIn = !that.data.activities[0].isIn;
-
-    console.log(that.data.activities[0].isIn);
-  },
-
-  // 绑定分类选择器的函数
+  // 绑定分类选择器
   bindTagPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+    console.log('picker发送选择改变，携带值为', e.detail.value);
     this.setData({
       idxOfTag: e.detail.value,
-    })
+    });
     this.setData({
       classification: '分类：' + this.data.tag[this.data.idxOfTag],
-    })
-    console.log('分类为', this.data.classification)
+    });
+    console.log('分类为', this.data.classification);
+    // TODO: 传idOfTag到后端进行分类
+    // ......
   },
 
-  // 绑定排序选择器的函数
+  // 绑定排序选择器
   bindSortPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       idxOfSort: e.detail.value,
-    })
+    });
     this.setData({
       order: '排序：' + this.data.sort[this.data.idxOfSort],
-    })
-    console.log('排序为', this.data.order)
+    });
+    console.log('排序为', this.data.order);
+    // TODO: 传idOfSort到后端进行排序
+    // ......
+  },
+
+  // 搜索输入
+  SearchInput: function (e) {
+    console.log("搜索输入：" + e.detail.value);
+    this.setData({
+      SearchData: e.detail.value
+    });
+  },
+
+  // 搜索提交
+  SearchConfirm: function () {
+    console.log("搜索提交：" + this.data.SearchData);
+    // TODO: 传SearchData到后端进行搜索
+    // ......
+  },
+
+  // 搜索清除
+  SearchClear: function () {
+    console.log("搜索清除...");
+    this.setData({
+      SearchData: ''
+    });
   },
 })
 
